3128824b2b635647a7ff0a58f8b3e46c500f59b6
sungyun6789 <sungyun5423@gmail.com>
2023-05-21 18:24:19 +0900

    올림픽 통계 서비스 최적화

--- lecture-2/src/App.js
+++ lecture-2/src/App.js
@@ -1,13 +1,34 @@
-import React, { useState } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 import InfoTable from "./components/InfoTable";
 import SurveyChart from "./components/SurveyChart";
 import Footer from "./components/Footer";
-import ImageModal from "./components/ImageModal";
+// import ImageModal from "./components/ImageModal";
+
+const LazyImageModal = lazy(() => import("./components/ImageModal"));
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+
+  /**
+   * preloading
+   *
+   * dynamic import는 필요한 순간에 불러오는 기술이고 preloading은 사전에 미리 불러오는 기술
+   */
+  const handleMouseEnter = () => {
+    const component = import("./components/ImageModal");
+  };
+
+  useEffect(() => {
+    // 컴포넌트 마운트 이후에 preloading 하는 방법
+    const component = import("./components/ImageModal");
+
+    // 이미지 사전 로딩
+    const img = new Image();
+    img.src =
+      "https://stillmed.olympic.org/media/Photos/2016/08/20/part-1/20-08-2016-Football-Men-01.jpg?interpolation=lanczos-none&resize=*:800";
+  }, []);
 
   return (
     <div className="App">
@@ -17,18 +38,21 @@ function App() {
         onClick={() => {
           setShowModal(true);
         }}
+        onMouseEnter={handleMouseEnter}
       >
         올림픽 사진 보기
       </ButtonModal>
       <SurveyChart />
       <Footer />
-      {showModal ? (
-        <ImageModal
-          closeModal={() => {
-            setShowModal(false);
-          }}
-        />
-      ) : null}
+      <Suspense fallback={null}>
+        {showModal ? (
+          <LazyImageModal
+            closeModal={() => {
+              setShowModal(false);
+            }}
+          />
+        ) : null}
+      </Suspense>
     </div>
   );
 }
