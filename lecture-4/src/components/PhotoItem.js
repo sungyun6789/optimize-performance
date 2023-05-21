4f54edab922d5b4c0bf952c2ee22553ea1494b15
sungyun6789 <sungyun5423@gmail.com>
2023-05-22 03:17:19 +0900

    이미지 갤러리 최적화

--- lecture-4/src/components/PhotoItem.js
+++ lecture-4/src/components/PhotoItem.js
@@ -1,31 +1,47 @@
 import React from "react";
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
-import { showModal } from "../redux/imageModal";
+import { setBgColor, showModal } from "../redux/imageModal";
+import LazyLoad from "react-lazyload";
+import { getAverageColorOfImage } from "../utils/getAverageColorOfImage";
 
 function PhotoItem({ photo: { urls, alt } }) {
   const dispatch = useDispatch();
 
-  const openModal = () => {
+  const openModal = (e) => {
     dispatch(showModal({ src: urls.full, alt }));
+
+    // 섬네일 이미지로 배경색 계산 후 리덕스에 저장
+    const averageColor = getAverageColorOfImage(e.target);
+    dispatch(setBgColor(averageColor));
   };
 
   return (
     <ImageWrap>
-      <Image
-        src={urls.small + "&t=" + new Date().getTime()}
-        alt={alt}
-        onClick={openModal}
-      />
+      <LazyLoad offset={1000}>
+        <Image
+          src={urls.small + "&t=" + new Date().getTime()}
+          alt={alt}
+          onClick={openModal}
+        />
+      </LazyLoad>
     </ImageWrap>
   );
 }
 
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  width: 100%;
+  padding-bottom: 56.25%;
+  position: relative;
+`;
 
 const Image = styled.img`
   cursor: pointer;
   width: 100%;
+  position: absolute;
+  hegiht: 100%;
+  top: 0;
+  left: 0;
 `;
 
 export default PhotoItem;
