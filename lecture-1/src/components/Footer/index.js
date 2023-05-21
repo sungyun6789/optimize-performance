0f8be2abefb544378774302f6d1b933ca3e814a5
sungyun6789 <sungyun5423@gmail.com>
2023-05-21 12:00:14 +0900

    블로그 서비스 프로젝트 추가

---
+++ lecture-1/src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+
+function Footer() {
+  return (
+    <div className={"Footer"}>©Performance Optimization Lecture for React</div>
+  );
+}
+
+export default Footer;
